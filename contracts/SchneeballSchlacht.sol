1633b2ea0a70c531ce96007d5da08f3f69b67105
Sono Willbrand <sono00@web.com>
2022-06-21 11:01:25 +0200

    feat: specify feature

--- contracts/SchneeballSchlacht.sol
+++ contracts/SchneeballSchlacht.sol
@@ -1,13 +1,18 @@
 pragma solidity 0.8.15;
 
 import "./ERC721Round/ERC721Round.sol";
+import "./ISchneeballSchlacht.sol";
 
 // TODO: payout
 // TODO: maybe event
 // TODO: refactor transfer to throw func
 // TODO: refactor
 // TODO: calc ban hit when throwing snowball
-contract SchneeballSchlacht is ERC721Round {
+contract SchneeballSchlacht is ISchneeballSchlacht, ERC721Round {
+    uint8 private constant MAX_LEVEL = 20;
+    uint256 private constant MINT_FEE = 0.1 ether;
+    uint256 private constant TRANSFER_FEE = 0.001 ether;
+
     // Mapping from snowball ID to snowball
     mapping(uint256 => mapping(uint256 => Snowball)) private _snowballs;
 
@@ -18,14 +23,14 @@ contract SchneeballSchlacht is ERC721Round {
     }
 
     modifier checkFee(uint256 tokenId) {
-        uint256 roundId = _roundIdCounter.current();
+        uint256 roundId = getRoundId();
         uint8 level = _snowballs[roundId][tokenId].level;
         require(msg.value == TRANSFER_FEE * level, "Insufficient fee!");
         _;
     }
 
     modifier isTransferable(uint256 tokenId, address to) {
-        uint256 roundId = _roundIdCounter.current();
+        uint256 roundId = getRoundId();
         uint8 level = _snowballs[roundId][tokenId].level;
         uint256 amountOfPartners = _snowballs[roundId][tokenId].partners.length;
         require(
@@ -41,13 +46,39 @@ contract SchneeballSchlacht is ERC721Round {
 
     event LevelUp(uint256 indexed roundId, uint256 indexed tokenId);
 
-    constructor() ERC721Group("SchneeballSchlacht", "Schneeball") {}
+    constructor() ERC721Round("SchneeballSchlacht", "Schneeball") {}
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        uint256 roundId = getRoundId();
+        return
+            string(
+                abi.encodePacked(_baseURI(), _snowballs[roundId][tokenId].level)
+            );
+    }
+
+    function tokenURI(uint256 roundId, uint256 tokenId)
+        external
+        view
+        override
+        returns (string memory)
+    {
+        return
+            string(
+                abi.encodePacked(_baseURI(), _snowballs[roundId][tokenId].level)
+            );
+    }
 
     function mint(address to) public payable checkDeadline {
         require(msg.value == MINT_FEE, "Insufficient fee!");
-        uint256 roundId = _roundIdCounter.current();
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
+        uint256 roundId = getRoundId();
+        uint256 tokenId = getTokenId();
+        incrementTokenId();
         _safeMint(to, tokenId);
         Snowball memory snowball = Snowball({
             level: 1,
@@ -62,7 +93,7 @@ contract SchneeballSchlacht is ERC721Round {
         view
         returns (uint256[] memory)
     {
-        uint256 roundId = _roundIdCounter.current();
+        uint256 roundId = getRoundId();
         uint256 amountOfPartners = _snowballs[roundId][tokenId].partners.length;
         uint256[] memory partners = new uint256[](amountOfPartners);
         for (uint256 i = 0; i < amountOfPartners; i++) {
@@ -88,7 +119,7 @@ contract SchneeballSchlacht is ERC721Round {
      * @dev Returns level from the current round.
      */
     function getLevel(uint256 tokenId) public view returns (uint8) {
-        uint256 roundId = _roundIdCounter.current();
+        uint256 roundId = getRoundId();
         uint8 level = _snowballs[roundId][tokenId].level;
         return level;
     }
@@ -105,9 +136,9 @@ contract SchneeballSchlacht is ERC721Round {
     function toss(address to, uint256 tokenId)
         public
         payable
-        checkToken(tokenId)
-        checkFee(tokenId)
         checkDeadline
+        checkFee(tokenId)
+        checkToken(tokenId)
         isTransferable(tokenId, to)
     {
         require(
@@ -115,9 +146,9 @@ contract SchneeballSchlacht is ERC721Round {
             "ERC721: transfer from incorrect owner"
         );
         require(to != address(0), "ERC721: transfer to the zero address");
-        uint256 roundId = _roundIdCounter.current();
-        uint256 newTokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
+        uint256 roundId = getRoundId();
+        uint256 newTokenId = getTokenId();
+        incrementTokenId();
 
         _safeMint(to, newTokenId);
 
@@ -136,8 +167,9 @@ contract SchneeballSchlacht is ERC721Round {
         }
     }
 
+    // TODO: When level up increment tokenId => mint new token
     function levelup(uint256 tokenId) internal {
-        uint256 roundId = _roundIdCounter.current();
+        uint256 roundId = getRoundId();
         uint256 amountOldPartners = _snowballs[roundId][tokenId]
             .partners
             .length;
@@ -173,4 +205,15 @@ contract SchneeballSchlacht is ERC721Round {
         );
         return uint256(hashValue) % length;
     }
+
+    function startRound() external override(ERC721Round, ISchneeballSchlacht) {}
+
+    function endRound() external override(ERC721Round, ISchneeballSchlacht) {}
+
+    function totalSupply()
+        external
+        view
+        override(ERC721Round, ISchneeballSchlacht)
+        returns (uint256)
+    {}
 }
