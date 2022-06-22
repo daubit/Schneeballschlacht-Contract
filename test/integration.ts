1af9238c4a1293fce79029fee165b9ac739e7a81
Sono Willbrand <sono00@web.com>
2022-06-22 16:32:45 +0200

    feat: add integration test

--- test/integration.ts
+++ test/integration.ts
@@ -1,93 +1,128 @@
-/* eslint-disable object-shorthand */
-/* eslint-disable no-unused-vars */
-/* eslint-disable node/no-unsupported-features/es-builtins */
 /* eslint-disable node/no-missing-import */
+/* eslint-disable object-shorthand */
 // We require the Hardhat Runtime Environment explicitly here. This is optional
 // but useful for running the script in a standalone fashion through `node <script>`.
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
+import { randomInt } from "crypto";
+import { BigNumber, Contract } from "ethers";
 import { parseEther } from "ethers/lib/utils";
 import { writeFileSync } from "fs";
 import { ethers } from "hardhat";
 
 const TRANSFER_FEE = (level: number) => parseEther((0.001 * level).toFixed(5));
-const errors: any = [];
+const MINT_FEE = parseEther("0.1");
+const partners: { [tokenId: number]: string[] } = {};
+const addresses: string[] = [];
 
-// TODO:
-//	Catch error
-// 	View Balance of Contract
-//	Question owned token upgrade
-// 	Block user
-// 	transfers => throws. TokenIds used in Game can be transfered
-//	Investigate double trans error
-// 	Investigate insufficient fee error when at level 9
+async function hasTokens(contract: Contract, address: string) {
+  const balance = await contract["balanceOf(address)"](address);
+  return Number(balance) > 0;
+}
+
+async function getLevel(contract: Contract, tokenId: number | BigNumber) {
+  return Number(await contract.functions["getLevel(uint256)"](tokenId));
+}
+
+async function getMaxToken(contract: Contract, address: string) {
+  const tokens = await contract["getTokensOfAddress(address)"](address);
+  let maxToken;
+  let tmpLevel = 0;
+  for (const token of tokens) {
+    const level = await getLevel(contract, token);
+    if (tmpLevel < level) {
+      tmpLevel = level;
+      maxToken = token;
+    }
+  }
+  return { token: maxToken, level: tmpLevel };
+}
+
+function getRandomAddress(currentAddress: string, tokenId: number) {
+  const randIndex = randomInt(100);
+  let randAddress = addresses[randIndex];
+  if (!partners[tokenId]) {
+    partners[tokenId] = [];
+  }
+  while (
+    randAddress !== currentAddress ||
+    partners[tokenId].includes(randAddress)
+  ) {
+    const randIndex = randomInt(100);
+    randAddress = addresses[randIndex];
+  }
+  return randAddress;
+}
+
+function addPartner(tokenId: number, address: string) {
+  if (partners[tokenId]) {
+    partners[tokenId].push(address);
+  } else {
+    partners[tokenId] = [address];
+  }
+}
+
 async function main() {
   const wallets = await ethers.getSigners();
-  const addresses = [];
   for (const wallet of wallets) {
     addresses.push(await wallet.getAddress());
   }
-  const iterator = (start: number) => {
-    let value = start;
-    return () => ++value % addresses.length;
-  };
-
-  let currentAddress = addresses[0];
-  let currentToken = 1;
-  console.log(`Amount of wallets: ${wallets.length}`);
   const Schneeball = await ethers.getContractFactory("SchneeballSchlacht");
   const schneeball = await Schneeball.deploy();
+  console.log("Contract deployed!");
   const startTx = await schneeball.startRound();
   await startTx.wait();
-
-  console.log("Contract deployed!");
-  for (let level = 1; level < 20; level++) {
-    const next = iterator(addresses.indexOf(currentAddress));
-    // Transfering Token
-    for (let i = 0; i <= level; i++) {
-      const nextIndex = next();
-      try {
+  console.log("Round started!");
+  while (true) {
+    try {
+      const randIndex = randomInt(addresses.length);
+      const currentAddress = addresses[randIndex];
+      const signer = await ethers.getSigner(currentAddress);
+      if (await hasTokens(schneeball, currentAddress)) {
+        const { token, level } = await getMaxToken(schneeball, currentAddress);
+        const maxToken = token!.toNumber();
+        const randAddress = getRandomAddress(currentAddress, maxToken);
+        addPartner(maxToken, randAddress);
         const transferTx = await schneeball
-          .connect(await ethers.getSigner(currentAddress))
-          .toss(addresses[nextIndex], currentToken, {
-            value: TRANSFER_FEE(
-              (
-                await schneeball.functions["getLevel(uint256)"](currentToken)
-              )[0]
-            ),
+          .connect(signer)
+          .toss(randAddress, maxToken, {
+            value: TRANSFER_FEE(level),
           });
         await transferTx.wait();
-      } catch (e) {
-        console.log("Oh no, an error occurred!");
-        errors.push(e);
-      }
-    }
-    const total = Number(await schneeball.functions["totalSupply()"]());
-    // Lookup upgraded Token
-    for (let i = currentToken; i <= total; i++) {
-      const nextLevel = Number(
-        await schneeball.functions["getLevel(uint256)"](i)
-      );
-      if (level + 1 === nextLevel) {
-        currentToken = i;
-        currentAddress = (
-          await schneeball.functions["ownerOf(uint256)"](currentToken)
-        )[0];
         console.log(
-          `TokenId ${currentToken} of ${currentAddress} has upgraded to ${nextLevel}!`
+          `${currentAddress} tossed to ${randAddress} with level ${level}`
         );
-        break;
+      } else {
+        console.log(`${currentAddress} minted...`);
+        const mintTx = await schneeball.connect(signer).mint(currentAddress, {
+          value: MINT_FEE,
+        });
+        await mintTx.wait();
       }
+    } catch (e: any) {
+      console.log({ e });
+      if (e.reason && e.reason.includes("Round has finished")) {
+        cleanup();
+      }
+      break;
     }
   }
-  writeFileSync("data/error.json", JSON.stringify(errors, null, 2));
 
-  const total = Number(await schneeball.functions["totalSupply()"]());
+  const total = Number(await schneeball["totalSupply()"]());
   const addressData: { [address: string]: any[] } = {};
+  const roundData: any = {};
+  const winner = await schneeball["getWinner()"]();
+  const payout = await schneeball["getPayout()"]();
+  const tosses = await schneeball["totalTosses()"]();
+  roundData.winner = winner;
+  roundData.payout = Number(payout);
+  roundData.tosses = Number(tosses);
+  writeFileSync("data/round.json", JSON.stringify(roundData, null, 2));
+
   for (let i = 1; i <= total; i++) {
-    const owner = (await schneeball.functions["ownerOf(uint256)"](i))[0];
-    const level = Number(await schneeball.functions["getLevel(uint256)"](i));
+    const owner = (await schneeball["ownerOf(uint256)"](i))[0];
+    const level = await getLevel(schneeball, i);
     const entry = { tokenId: i, level: level };
     if (addressData[owner]) {
       addressData[owner].push(entry);
@@ -96,6 +131,10 @@ async function main() {
     }
   }
   writeFileSync("data/tokens.json", JSON.stringify(addressData, null, 2));
+}
+
+function cleanup() {
+  // TODO: Test lock on transer, mint, toss
 }
 
 // We recommend this pattern to be able to use async/await everywhere
