c3c7b89edcf9691be1982835d80468b74f1b3615
Sono Willbrand <sono00@web.com>
2022-06-23 16:11:26 +0200

    feat: add deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,25 +1,34 @@
+/* eslint-disable node/no-missing-import */
 // We require the Hardhat Runtime Environment explicitly here. This is optional
 // but useful for running the script in a standalone fashion through `node <script>`.
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
+import { Storage } from "./storage";
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
-
+  const network = await ethers.provider.getNetwork();
+  const storage = new Storage("contracts/addresses.json");
+  let { sbs: sbsAddress, hof: hofAddress } = storage.fetch(network.chainId);
+  const addresses: any = {};
   // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
-
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+  if (!hofAddress) {
+    const HOF = await ethers.getContractFactory("HallOfFame");
+    const hof = await HOF.deploy();
+    await hof.deployed();
+    addresses.hof = hof.address;
+    hofAddress = hof.address;
+    console.log("Hall of Fame deployed to:", hof.address);
+  }
+  if (!sbsAddress) {
+    const Sbs = await ethers.getContractFactory("SchneeballSchlacht");
+    const sbs = await Sbs.deploy(hofAddress);
+    await sbs.deployed();
+    addresses.sbs = sbs.address;
+    console.log("SchneeballSchlacht deployed to:", sbs.address);
+  }
+  storage.save(network.chainId, addresses);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
