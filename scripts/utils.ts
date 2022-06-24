ef36b6990401040683207af82ce87f7bfa9b30ec
Sono Willbrand <sono00@web.com>
2022-06-24 10:36:29 +0200

    adjust fees from contract

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -1,6 +1,6 @@
 import { parseEther } from "ethers/lib/utils";
 
 export const TOSS_FEE = (level: number) =>
-  parseEther((0.001 * level).toFixed(10));
+  parseEther((0.01 * level).toFixed(10));
 
-export const MINT_FEE = parseEther("0.1");
+export const MINT_FEE = parseEther("0.05");
