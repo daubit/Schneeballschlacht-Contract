4ceb08bb7577b56018589c68492dd039ea13ef4c
Alexander Krahl <alexander.krahl@daubit.org>
2022-07-12 16:10:36 +0200

    add hof tests

--- contracts/HallOfFame.sol
+++ contracts/HallOfFame.sol
@@ -5,6 +5,7 @@ pragma solidity 0.8.15;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
+// TODO: tokenURI links
 contract HallOfFame is ERC721, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
---
+++ test/hof.ts
@@ -0,0 +1,31 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { expect } from "chai";
+import { Contract } from "ethers";
+import { ethers } from "hardhat";
+
+describe("Schneeballschlacht - HOF", async () => {
+  let hof: Contract;
+  // eslint-disable-next-line no-unused-vars
+  let users: SignerWithAddress[];
+
+  describe("Setup + Functions", () => {
+    before(async () => {
+      // Setting up accounts
+      users = await ethers.getSigners();
+
+      // Deploy Schneeballschlacht
+      const HOF = await ethers.getContractFactory("HallOfFame");
+      hof = await HOF.connect(users[0]).deploy();
+      await hof.deployed();
+    });
+    it("can start successfully", async () => {
+      const contractURI = await hof.contractURI();
+      expect(contractURI).to.be.equals("");
+      await expect(hof.connect(users[1]).mint(users[0].address)).to.reverted;
+      const mintTx = await hof.connect(users[0]).mint(users[2].address);
+      await mintTx.wait();
+      expect(await hof.ownerOf(1)).to.be.equals(users[2].address);
+      expect(await hof.tokenURI(1)).to.be.equals("ipfs://1");
+    });
+  });
+});
