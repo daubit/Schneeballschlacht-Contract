a5ee02f1b53413171c41f8894d09d6e0b1971417
Sono Willbrand <sono.willbrand@daubit.org>
2022-08-12 11:31:27 +0200

    add query test

--- contracts/test/SchneeballSchlachtMaxLevel3.sol
+++ contracts/test/SchneeballSchlachtMaxLevel3.sol
@@ -6,4 +6,15 @@ import "../Schneeballschlacht.sol";
 
 contract SchneeballSchlachtMaxLevel3 is Schneeballschlacht {
     constructor(address hof) Schneeballschlacht(hof, 3) {}
+
+    /**
+     * @dev Function to mint a snowball
+     *
+     * @param to address to send the snowball to
+     */
+    function mint(address to, uint256 amount) external {
+        for (uint256 i; i < amount; i++) {
+            _mint(to, 1, 0);
+        }
+    }
 }
