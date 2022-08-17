bb2f1de42aca0aacf2fa79152a76a93ecd0e20e9
Sono Willbrand <sono.willbrand@daubit.org>
2022-08-17 17:00:47 +0200

    add opensea integration

--- contracts/HallOfFame.sol
+++ contracts/HallOfFame.sol
@@ -4,9 +4,24 @@ pragma solidity 0.8.16;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "./common/meta-transactions/ContentMixin.sol";
+import "./common/meta-transactions/NativeMetaTransaction.sol";
 
-contract HallOfFame is ERC721, AccessControl {
+contract OwnableDelegateProxy {}
 
+/**
+ * Used to delegate ownership of a contract to another address, to save on unneeded transactions to approve contract use for users
+ */
+contract ProxyRegistry {
+    mapping(address => OwnableDelegateProxy) public proxies;
+}
+
+contract HallOfFame is
+    ERC721,
+    AccessControl,
+    NativeMetaTransaction,
+    ContextMixin
+{
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     uint256 private _totalSupply;
@@ -14,11 +29,18 @@ contract HallOfFame is ERC721, AccessControl {
     string private _contractURI;
     string private _baseURIS;
 
-    constructor(string memory contractURIc, string memory baseURIc) ERC721("HallOfFame", "Trophy") {
+    address private _proxyRegistryAddress;
+
+    constructor(
+        string memory contractURI_,
+        string memory baseURI,
+        address proxyRegistryAddress
+    ) ERC721("HallOfFame", "Trophy") {
         _grantRole(MINTER_ROLE, msg.sender);
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _contractURI = contractURIc;
-        _baseURIS = baseURIc;
+        _contractURI = contractURI_;
+        _baseURIS = baseURI;
+        _proxyRegistryAddress = proxyRegistryAddress;
     }
 
     function _baseURI() internal view override returns (string memory) {
@@ -27,6 +49,24 @@ contract HallOfFame is ERC721, AccessControl {
 
     function contractURI() external view returns (string memory) {
         return _contractURI;
+    }
+
+    /**
+     * @dev Override isApprovedForAll to whitelist user's OpenSea proxy accounts to enable gas-less listings.
+     */
+    function isApprovedForAll(address owner, address operator)
+        public
+        view
+        override
+        returns (bool)
+    {
+        // Whitelist OpenSea proxy contract for easy trading.
+        ProxyRegistry proxyRegistry = ProxyRegistry(_proxyRegistryAddress);
+        if (address(proxyRegistry.proxies(owner)) == operator) {
+            return true;
+        }
+
+        return super.isApprovedForAll(owner, operator);
     }
 
     function supportsInterface(bytes4 interfaceId)
@@ -45,10 +85,27 @@ contract HallOfFame is ERC721, AccessControl {
         _safeMint(to, _totalSupply);
     }
 
-    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
-        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
 
         string memory baseURI = _baseURI();
-        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI)) : "";
+        return
+            bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI)) : "";
+    }
+
+    /**
+     * @dev This is used instead of msg.sender as transactions won't be sent by the original token owner, but by OpenSea.
+     */
+    function _msgSender() internal view override returns (address sender) {
+        return ContextMixin.msgSender();
     }
 }
