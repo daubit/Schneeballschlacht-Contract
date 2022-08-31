bd66eaa641898e4ef11c9883d1b94c01d0299970
Alexander Krahl <alexander.krahl@daubit.org>
2022-08-31 09:49:36 +0200

    add local node to config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -35,6 +35,12 @@ const config: HardhatUserConfig = {
   },
 
   networks: {
+    hardhatnode: {
+      url: "http://127.0.0.1:8545/",
+      accounts: [
+        "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+      ],
+    },
     mumbai: { url: mumbaiNodeUrl, accounts: { mnemonic: MNEMONIC } },
     polygon: { url: polygonNodeUrl, accounts: { mnemonic: MNEMONIC } },
     evmos: { url: evmosNodeUrl, accounts: { mnemonic: MNEMONIC } },
